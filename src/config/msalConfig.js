fab4f77f97e0e031d7a61258bdd7603766dc9471
Yasser Bouaabane <b.yasser@Alexsys.solutions>
2025-04-15 18:44:03 +0100

    .

--- src/config/msalConfig.js
+++ src/config/msalConfig.js
@@ -1,10 +1,10 @@
 export const msalConfig = {
   auth: {
-    clientId: '97b38229-b8c2-43e8-b6ed-604167894be1', // This is the ONLY mandatory field that you need to supply.
-    authority: 'https://TetouanB2C.b2clogin.com/TetouanB2C.onmicrosoft.com/B2C_1_ppn', // Choose SUSI as your default authority.
-    knownAuthorities: ['TetouanB2C.b2clogin.com'], // Mark your B2C tenant's domain as trusted.
-    redirectUri: 'http://localhost:3000/', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
-    postLogoutRedirectUri: 'http://localhost:3000/', // Indicates the page to navigate after logout.
+    clientId: 'f74e0fe0-cb51-4f56-9c51-113448078d3f', // This is the ONLY mandatory field that you need to supply.
+    authority: 'https://b2ceducaiami.b2clogin.com/b2ceducaiami.onmicrosoft.com/B2C_1_sonasid', // Choose SUSI as your default authority.
+    knownAuthorities: ['b2ceducaiami.b2clogin.com'], // Mark your B2C tenant's domain as trusted.
+    redirectUri: 'http://localhost:3000', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
+    postLogoutRedirectUri: 'http://localhost:3000', // Indicates the page to navigate after logout.
     navigateToLoginRequestUrl: false, // If 'true', will navigate back to the original request location before processing the auth code response.
   },
   cache: {
